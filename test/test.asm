2e796dbcca0742c47bd6431ded6216fbaabedbbd
Alexandre Chamard-bois <alexandre.chamard-bois@epitech.eu>
2017-11-18 12:32:18 +0100

    Doc registre + .DATA

--- test/test.asm
+++ test/test.asm
@@ -1,73 +1,73 @@
 ;Defines
-SYS_EXIT  	equ 1
-SYS_READ  	equ 3
-SYS_WRITE 	equ 4
-STDIN     	equ 0
-STDOUT    	equ 1
+SYS_EXIT	equ 1
+SYS_READ	equ 3
+SYS_WRITE	equ 4
+STDIN		equ 0
+STDOUT		equ 1
 EXIT_S		equ 0
 EXIT_F		equ 84
 BUFF_SIZE	equ 1024
-READ_SIZE       equ 1
+READ_SIZE	equ 1
 
-section .data                                   ;Data segment
-        userPrompt db 'Please enter: '         ;Prompt
-        lenPrompt equ $-userPrompt             	;???
-        dispMsg db 'You have entered: '         ;Response
-        lenMsg equ $-dispMsg                	;???
-        stars db '****', 0x0A                   ;Stars
-        lenStars equ $-stars                    ;???
+section .data							;Data segment
+	userPrompt	db	'Please enter: '	;Prompt
+	lenPrompt	equ	$-userPrompt		;???
+	dispMsg		db	'You have entered: ';Response
+	lenMsg		equ	$-dispMsg			;???
+	stars		db	'****', 0x0A		;Stars
+	lenStars	equ	$-stars				;???
 
-section .bss                    ;Uninitialized data (static vars) 
-        lenInput resb BUFF_SIZE ;???
-        iterator resb 1
-	
-section .text                   ;Code Segment
-        global _start
-	
+section .bss							;Uninitialized data (static vars)
+	lenInput	resb	BUFF_SIZE		;???
+	iterator	resb	1
+
+section .text							;Code Segment
+	global _start
+
 _start:
-        ;Write stars
-        mov eax, SYS_WRITE      ;eax serve here for storing the number of the syscall (here, write)
-        mov ebx, STDOUT         ;fd (stdout)
-        mov ecx, stars          ;pointer of stars to write
-        mov edx, lenStars       ;len of stars to write
-        int 0x80                ;syscall
-        
-        ;Write prompt
-        mov eax, SYS_WRITE      ;syscall write
-        mov ebx, STDOUT         ;fd (stdout)
-        mov ecx, userPrompt     ;pointer for message to write
-        mov edx, lenPrompt     	;len of the message to write
-        int 0x80                ;calling the syscall with interrupt
+										;Write stars
+	mov eax, SYS_WRITE					;eax serve here for storing the number of the syscall (here, write)
+	mov ebx, STDOUT						;fd (stdout)
+	mov ecx, stars						;pointer of stars to write
+	mov edx, lenStars					;len of stars to write
+	int 0x80							;syscall
 
-        ;Read and store input
-        mov eax, SYS_READ       ;read syscall number
-        mov ebx, STDIN          ;fd (stdin)
-        mov ecx, lenInput       ;pointer to store read bytes
-        mov edx, READ_SIZE      ;nb of bytes to read
-        int 0x80                ;syscall
-	
-        ;Write response
-        mov eax, SYS_WRITE      ;syscall write
-        mov ebx, STDOUT         ;fd (stdout)
-        mov ecx, dispMsg        ;pointer
-        mov edx, lenMsg     	;len to write
-        int 0x80                ;syscall
+;Write prompt
+	mov eax, SYS_WRITE					;syscall write
+	mov ebx, STDOUT						;fd (stdout)
+	mov ecx, userPrompt					;pointer for message to write
+	mov edx, lenPrompt					;len of the message to write
+	int 0x80							;calling the syscall with interrupt
 
-        ;Output what was entered
-        mov eax, SYS_WRITE      ;syscall write
-        mov ebx, STDOUT         ;fd (stdout)
-        mov ecx, lenInput       ;pointer
-        mov edx, BUFF_SIZE      ;len to write
-        int 0x80                ;syscall
-    
-        ;Write stars
-        mov eax, SYS_WRITE      ;eax serve here for storing the number of the syscall (here, write)
-        mov ebx, STDOUT         ;fd (stdout)
-        mov ecx, stars          ;pointer of stars to write
-        mov edx, lenStars       ;len of stars to write
-        int 0x80                ;syscall
+;Read and store input
+	mov eax, SYS_READ					;read syscall number
+	mov ebx, STDIN						;fd (stdin)
+	mov ecx, lenInput					;pointer to store read bytes
+	mov edx, READ_SIZE					;nb of bytes to read
+	int 0x80							;syscall
 
-        ; Exit code
-        mov eax, SYS_EXIT       ;syscall exit
-        mov ebx, EXIT_S         ;return value
-        int 0x80                ;syscall
+;Write response
+	mov eax, SYS_WRITE					;syscall write
+	mov ebx, STDOUT						;fd (stdout)
+	mov ecx, dispMsg					;pointer
+	mov edx, lenMsg						;len to write
+	int 0x80							;syscall
+
+;Output what was entered
+	mov eax, SYS_WRITE					;syscall write
+	mov ebx, STDOUT						;fd (stdout)
+	mov ecx, lenInput					;pointer
+	mov edx, BUFF_SIZE					;len to write
+	int 0x80							;syscall
+
+;Write stars
+	mov eax, SYS_WRITE					;eax serve here for storing the number of the syscall (here, write)
+	mov ebx, STDOUT						;fd (stdout)
+	mov ecx, stars						;pointer of stars to write
+	mov edx, lenStars					;len of stars to write
+	int 0x80							;syscall
+
+;Exit code
+	mov eax, SYS_EXIT					;syscall exit
+	mov ebx, EXIT_S						;return value
+	int 0x80							;syscall
--- test/test2.asm
+++ test/test2.asm
@@ -1,26 +1,27 @@
- section	.text
-   global _start        ;must be declared for using gcc
-	
-_start:	                ;tell linker entry point
-   mov ecx,10
-   mov eax, '1'
-	
+section	.text
+global _start			;must be declared for using gcc
+
+_start:					;tell linker entry point
+	mov ecx,10
+	mov eax, '1'
+
 l1:
-   mov [num], eax
-   mov eax, 4
-   mov ebx, 1
-   push ecx
-	
-   mov ecx, num 
-   mov edx, 1
-   int 0x80
-	
-   mov eax, [num]
-   inc eax
-   pop ecx
-   loop l1
-	
-   mov eax,1             ;system call number (sys_exit)
-   int 0x80              ;call kernel
+	mov [num], eax
+	mov eax, 4
+	mov ebx, 1
+	push ecx
+
+	mov ecx, num
+	mov edx, 1
+	int 0x80
+
+	mov eax, [num]
+	inc eax
+	pop ecx
+	loop l1
+
+	mov eax,1			;system call number (sys_exit)
+	int 0x80			;call kernel
+
 section	.bss
 num resb 1
