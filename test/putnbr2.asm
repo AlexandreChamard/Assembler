ef0dffb4300ff467c75e0b13d86aecb16f549fd5
FulcrandG <guilhem.fulcrand@epitech.eu>
2017-11-23 11:32:45 +0100

    [FIX]putnbr2.asm: still not functional, added some testing messages

--- test/putnbr2.asm
+++ test/putnbr2.asm
@@ -1,6 +1,8 @@
 section .rodata
-	msg db "test", 0x0A
-	base_len equ 10			; Base numbers
+	;;; TO REMOVE
+	msg db "Nb to write : ", 0
+	end_line db 0x0A, 0
+	;;; TO REMOVE
 	ng db "-"               	; Negative symbol
 
 section .text
@@ -16,33 +18,44 @@ _putnbr:
 	push edx
 	push esi
 
-        mov eax, [ebp + 8]
+        mov eax, [ebp + 8]		; Get int given by _atoi
 	cmp eax, 0
-        jge calc_loop
-        ; if (nb < 0)
+        jge calc_loop			; if (nb < 0)
         push ng
         call _putstr
         pop esi
+	;;; TO REMOVE
+	push end_line
+	call _putstr
+	pop esi
+	;;; TO REMOVE
         neg eax				; Multiply eax by -1
 
 calc_loop:
+	;;; TO REMOVE
 	push msg
 	call _putstr
 	pop esi
-        cmp eax, base_len
+	;;; TO REMOVE
+        cmp eax, 10
         jl end_print
         xor edx, edx
-	mov ecx, base_len
+	mov ecx, 10
 	idiv ecx
 
 print_in_loop:
 	push eax
-	mov ecx, eax
-	;add ecx, '0'
+	mov ecx, edx			; Nb to write
+	add ecx, '0'
 	mov eax, 4			; sys_write
 	mov ebx, 1			; STDOUT
 	mov edx, 1
 	int 0x80
+	;;; TO REMOVE
+	push end_line
+	call _putstr
+	pop esi
+	;;; TO REMOVE
 	pop eax
 	jmp calc_loop
 
@@ -53,6 +66,11 @@ end_print:
 	mov ebx, 1			; STDOUT
 	mov edx, 1
 	int 0x80
+	;;; TO REMOVE
+	push end_line
+	call _putstr
+	pop esi
+	;;; TO REMOVE
 
 end_putnbr:
 	pop esi
