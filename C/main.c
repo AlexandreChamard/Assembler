459634221b289e014512f3027cefb5f95d459be7
Alexandre Chamard-bois <alexandre.chamard-bois@epitech.eu>
2017-12-03 19:14:40 +0100

    diff main 32 & 64

--- C/main.c
+++ C/main.c
@@ -1,10 +1,10 @@
-#include "include/libasm.h"
+// #include "include/libasm.h"
 
 int main(int ac, char **av)
 {
-	_printf("ac: %d\n", ac);
+	printf("ac: %d\n", ac);
 	for (int i = 0; i < ac; i++) {
-		_printf("av[%d]: %s\n", i, av[i]);
+		printf("av[%d]: %s\n", i, av[i]);
 	}
 	return (ac);
 }
