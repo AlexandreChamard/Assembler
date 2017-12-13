ace917f5d6e40bdc23a6a1dfddc5fbea747d4b5b
Alexandre Chamard-bois <alexandre.chamard-bois@epitech.eu>
2017-12-13 16:19:59 +0100

    merge

--- C/main.c
+++ C/main.c
@@ -1,9 +1,11 @@
-#include <stdio.h>
 #include "include/libasm.h"
 
 int main(int ac, char **av)
 {
-	if (_memcmp(NULL, av[2], 3))
+    if (ac < 4) {
+        return 1;
+    }
+	if (_wordcmp(av[1], av[2]))
                 _putstr("KO\n");
         else
                 _putstr("OK\n");
