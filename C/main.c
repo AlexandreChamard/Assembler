cc3ebde4e502945acf6d4262dba410bcee3a1ccf
Alexandre Chamard-bois <alexandre.chamard-bois@epitech.eu>
2017-12-13 13:59:58 +0100

    merge

--- C/main.c
+++ C/main.c
@@ -1,23 +1,8 @@
 #include "include/libasm.h"
 
-int my_atoi(char *str)
-{
-	int nb = 0;
-	int neg = 1;
-
-	if (*str == '-') {
-		neg = -1;
-		str++;
-	}
-	while (*str >= '0' && *str <= '9') {
-		nb = nb * 10 + *str - '0';
-		str++;
-	}
-	return (nb * neg);
-}
-
 int main(int ac, char **av)
 {
-	printf("%d\n", atoi(av[1]));
+    _putnbr(ac);
+    _putstr(*av);
 	return (0);
 }
