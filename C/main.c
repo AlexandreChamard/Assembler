fdf7644f093cd83c5afade3db05b8401ce762383
Guilhem Fulcrand <guilhem.fulcrand@epitech.eu>
2017-12-23 16:54:18 +0100

    [ADD]strdup.asm: strdup&strndup 64 added

--- C/main.c
+++ C/main.c
@@ -1,19 +1,9 @@
 #include "include/libasm.h"
-
 // #include <stdlib.h>
 
 int main(int ac, char **av)
 {
-    int *i = _malloc(sizeof(int));
-
-    *i = 0;
-    // *(i + 1) = 1;
-    // *(i - 1) = 2;
-    // for (int i = 0; i < 10; i++) {
-    // char *ptr = _malloc(_strlen(av[1]) + 1);
-    // _strcpy(ptr, av[1]);
-    // _puts(ptr);
-    // _puts(av[1]);
-    // }
-    return (0);
+        _printf("%s %c\n", av[1], ac);
+        return (0);
 }
+
